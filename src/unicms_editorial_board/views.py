37a8ac0fbda1e2a210727661a1d2b543cc9ce5af
Francesco Filicetti <francesco.filicetti@unical.it>
2021-03-31 22:26:20 +0200

    edit access permissions

--- src/unicms_editorial_board/views.py
+++ src/unicms_editorial_board/views.py
@@ -1,8 +1,13 @@
-from django.contrib.admin.views.decorators import staff_member_required
+# from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
 
-@staff_member_required
+# @staff_member_required
+@login_required
 def board_base(request):
+    if not request.user.is_staff:
+        raise PermissionDenied
     template = "board.html"
     return render(request, template)
